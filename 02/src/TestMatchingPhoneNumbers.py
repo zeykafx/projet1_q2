e152b4ddbf1ca359b0386ffbe0a9eefb6e234d3d
zeykafx <37271973+zeykafx@users.noreply.github.com>
2021-03-14 18:02:04 +0100

    Ajout d'un test random

--- 02/src/TestMatchingPhoneNumbers.py
+++ 02/src/TestMatchingPhoneNumbers.py
@@ -4,9 +4,24 @@
 import unittest
 import sys
 import os
+import random
 
 import CorrMatchingPhoneNumbers as correct
 import MatchingPhoneNumbers as student
+
+
+def gen_phone(numbers: int):
+    """
+    génère des numéros de téléphone random, pris et modifié de https://stackoverflow.com/a/26227853/14701142
+    """
+    generated_phone_numbers = []
+    for i in range(numbers):
+        first = random.randint(000, 499)
+        second = random.randint(10, 99)
+        third = random.randint(10, 99)
+        last = random.randint(10, 99)
+        generated_phone_numbers.append(f'0{first}/{second}.{third}.{last}')  # 0000/00.00.00
+    return generated_phone_numbers
 
 
 class TestMatchingPhoneNumber(unittest.TestCase):
@@ -18,8 +33,10 @@ class TestMatchingPhoneNumber(unittest.TestCase):
         except Exception as e:
             # au cas ou des exception viendrait du code de l'étudiant, on les catch et on les print (e.g: si l'étudiant n'a pas implémenté la fonction,...)
             self.fail(f"Votre fonction a provoqué l'exception {type(e)}: {e} avec comme argument {filename}")
-        answer = correct.is_phone_number_regex(filename)  # je prend la réponse que me donne ma fonction correcte et je compare l'égalité des deux réponses.
-        self.assertEqual(student_ans, answer, f"Votre fonction n'a pas retourné la bonne liste de numéros belge: vous avez retourné {student_ans} au lieu de {answer}, Veuillez à bien vérifier la positions des caractères spéciaux (e.g: '04' et les '/',...)")
+        answer = correct.is_phone_number_regex(
+            filename)  # je prend la réponse que me donne ma fonction correcte et je compare l'égalité des deux réponses
+        self.assertEqual(student_ans, answer,
+                         f"Votre fonction n'a pas retourné la bonne liste de numéros belge: vous avez retourné {student_ans} au lieu de {answer}, Veuillez à bien vérifier la positions des caractères spéciaux (e.g: '04' et les '/',...)")
 
     def test1_EmptyFile(self):
         filename = 'empty_file.txt'
@@ -28,7 +45,8 @@ class TestMatchingPhoneNumber(unittest.TestCase):
         except Exception as e:
             self.fail(f"Votre fonction a provoqué l'exception {type(e)}: {e} avec comme argument {filename}")
         answer = correct.is_phone_number_regex(filename)
-        self.assertEqual(student_ans, answer, f"Le fichier de test est vide et mais votre fonction à retourné {student_ans}")
+        self.assertEqual(student_ans, answer,
+                         f"Le fichier de test est vide et mais votre fonction à retourné {student_ans}")
 
     def test2_OneFalseNumber(self):
         filename = 'one_false_number.txt'
@@ -37,7 +55,8 @@ class TestMatchingPhoneNumber(unittest.TestCase):
         except Exception as e:
             self.fail(f"Votre fonction a provoqué l'exception {type(e)}: {e} avec comme argument {filename}")
         answer = correct.is_phone_number_regex(filename)
-        self.assertEqual(student_ans, answer, f"Le fichier de test contient un mauvais numéro dans ce cas, votre fonction à retourné {student_ans}")
+        self.assertEqual(student_ans, answer,
+                         f"Le fichier de test contient un mauvais numéro dans ce cas, votre fonction à retourné {student_ans}")
 
     def test3_OneTrueNumber(self):
         filename = 'one_true_number.txt'
@@ -46,7 +65,8 @@ class TestMatchingPhoneNumber(unittest.TestCase):
         except Exception as e:
             self.fail(f"Votre fonction a provoqué l'exception {type(e)}: {e} avec comme argument {filename}")
         answer = correct.is_phone_number_regex(filename)
-        self.assertEqual(student_ans, answer, f"Votre fonction n'a pas retourné la bonne liste de numéros belge: vous avez retourné {student_ans} au lieu de {answer}")
+        self.assertEqual(student_ans, answer,
+                         f"Votre fonction n'a pas retourné la bonne liste de numéros belge: vous avez retourné {student_ans} au lieu de {answer}")
 
     def test4_TwoTrueNumber(self):
         filename = 'two_true_numbers.txt'
@@ -55,8 +75,26 @@ class TestMatchingPhoneNumber(unittest.TestCase):
         except Exception as e:
             self.fail(f"Votre fonction a provoqué l'exception {type(e)}: {e} avec comme argument {filename}")
         answer = correct.is_phone_number_regex(filename)
-        self.assertEqual(student_ans, answer, f"Votre fonction n'a pas retourné la bonne liste de numéros belge: vous avez retourné {student_ans} au lieu de {answer}")
+        self.assertEqual(student_ans, answer,
+                         f"Votre fonction n'a pas retourné la bonne liste de numéros belge: vous avez retourné {student_ans} au lieu de {answer}")
 
+    def test5_random_numbers(self):
+        """
+        le test crée une liste de 100 numéros randoms qui vont donc servir de mesure pour empêcher l'étudiant de
+        "hard coder" les valeurs des autres fichiers
+        """
+        phone_numbers = gen_phone(100)
+        with open('test5.txt', "w") as gen_file:
+            for nbr in phone_numbers:
+                gen_file.write(nbr)
+        filename = 'test5.txt'
+        try:
+            student_ans = student.matching_phone_numbers(filename)
+        except Exception as e:
+            self.fail(f"Votre fonction a provoqué l'exception {type(e)}: {e} avec comme argument {filename}")
+        answer = correct.is_phone_number_regex(filename)
+        self.assertEqual(student_ans, answer,
+                         f"Votre fonction n'a pas retourné la bonne liste de numéros belge: vous avez retourné {student_ans} au lieu de {answer}")
 
 
 if __name__ == '__main__':
